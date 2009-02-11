1189ff81c723fa450071ed3744e3671d00819f3c
Dylan Stamat <dylanstamat@gmail.com>
2009-02-12 10:29:51 +1300

    add pluralization_index back to translations

--- generators/i18n_backend_database/templates/migrate/create_i18n_tables.rb
+++ generators/i18n_backend_database/templates/migrate/create_i18n_tables.rb
@@ -9,6 +9,7 @@ class CreateI18nTables < ActiveRecord::Migration
     create_table :translations do |t|
       t.string   :key
       t.string   :value
+      t.integer  :pluralization_index
       t.integer  :locale_id
     end
     add_index :translations, [:locale_id, :key]
