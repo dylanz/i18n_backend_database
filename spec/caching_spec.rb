9b081c490caa4657ae8fd10c35c86c6d7127a81a
Shane Mingins <smingins@Unknown-00-17-f2-e0-3f-0c.lan>
2009-02-13 14:41:10 +1300

    Added failing spec for caching all found translations when in non-default locale

--- spec/caching_spec.rb
+++ spec/caching_spec.rb
@@ -23,7 +23,7 @@ describe I18n::Backend::Database do
         I18n.locale = "en"
       end
 
-      it "should find lowest level translation" do
+      it "should cache translations" do
         @english_locale.translations.create!(:key => 'activerecord.errors.messages.blank', :value => 'is blank moron!')
       
         options = {:attribute=>"Locale", :value=>nil, 
@@ -38,13 +38,45 @@ describe I18n::Backend::Database do
         @backend.cache_store.read("en:#{Translation.hk("activerecord.errors.models.translation.attributes.locale.blank")}:1").should == nil
         @backend.cache_store.read("en:#{Translation.hk("activerecord.errors.models.translation.blank")}:1").should == nil
         @backend.cache_store.read("en:#{Translation.hk("activerecord.errors.messages.blank")}:1").should == "is blank moron!"
-
-
-        
       end
 
       
     end
+
+    describe "and locale es" do
+      
+      before(:each) do
+        I18n.locale = "es"
+        @spanish_locale = Locale.create!(:code => 'es')
+      end
+      
+      it "should cache translations" do
+        @english_locale.translations.create!(:key => 'activerecord.errors.messages.blank', :value => 'is blank moron!')
+      
+        options = {:attribute=>"Locale", :value=>nil, 
+          :scope=>[:activerecord, :errors], :default=>[:"models.translation.blank", :"messages.blank"], :model=>"Translation"}
+      
+        @backend.translate("es", :"models.translation.attributes.locale.blank", options).should == "is blank moron!"
+        
+        @backend.cache_store.exist?("es:#{Translation.hk("activerecord.errors.models.translation.attributes.locale.blank")}:1").should be_true
+        @backend.cache_store.exist?("es:#{Translation.hk("activerecord.errors.models.translation.blank")}:1").should be_true
+        @backend.cache_store.exist?("es:#{Translation.hk("activerecord.errors.messages.blank")}:1").should be_true
+
+        @backend.cache_store.read("es:#{Translation.hk("activerecord.errors.models.translation.attributes.locale.blank")}:1").should == nil
+        @backend.cache_store.read("es:#{Translation.hk("activerecord.errors.models.translation.blank")}:1").should == nil
+        @backend.cache_store.read("es:#{Translation.hk("activerecord.errors.messages.blank")}:1").should == "is blank moron!"
+
+        @backend.cache_store.exist?("en:#{Translation.hk("activerecord.errors.models.translation.attributes.locale.blank")}:1").should be_true
+        @backend.cache_store.exist?("en:#{Translation.hk("activerecord.errors.models.translation.blank")}:1").should be_true
+        @backend.cache_store.exist?("en:#{Translation.hk("activerecord.errors.messages.blank")}:1").should be_true
+
+        @backend.cache_store.read("en:#{Translation.hk("activerecord.errors.models.translation.attributes.locale.blank")}:1").should == nil
+        @backend.cache_store.read("en:#{Translation.hk("activerecord.errors.models.translation.blank")}:1").should == nil
+        @backend.cache_store.read("en:#{Translation.hk("activerecord.errors.messages.blank")}:1").should == "is blank moron!"
+      end
+      
+    end
+
 
   end
   
